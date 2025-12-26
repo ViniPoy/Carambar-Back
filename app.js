4dd060dc23c20f3350ad0e4bffa971360d842247
ViniPoy <vinipoy@gmail.com>
2025-12-26 15:09:00 +0100

    Ajout de la doc swagger dans app et routes.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const sequelize = require('./config/database');
 const Joke = require('./models/jokeModel');
 const jokeRoutes = require('./routes/jokeRoutes');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require ('swagger-ui-express');
 
 const app = express();
 
@@ -14,6 +16,27 @@ sequelize.sync({ force: false })
     .catch((err) => {
         console.error('Erreur de synchro : ', err);
     });
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Carambar & Co API',
+            version: '1.0.0',
+            description: 'API de blagues Carambar pour la Wild Code School'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000'
+            }
+        ]
+    },
+    apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/api/blagues', jokeRoutes)
 
--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -2,9 +2,56 @@ const express = require('express');
 const router = express.Router();
 const jokeController = require('../controllers/jokeController');
 
+/**
+ * @swagger
+ * /api/blagues:
+ *   post:
+ *     summary: Ajouter une blague
+ *     responses:
+ *       201:
+ *         description: Blague créée
+ */
 router.post('/', jokeController.createJoke);
+/**
+ * @swagger
+ * /api/blagues:
+ *   get:
+ *     summary: Liste toutes le blagues
+ *     responses:
+ *       200:
+ *         description: Succès
+ */
 router.get('/', jokeController.getAllJokes);
+/**
+ * @swagger
+ * /api/blagues/random:
+ *   get:
+ *     summary: Récupère une blague aléatoire
+ *     description: Renvoie une blague au hasard depuis la base de données
+ *     responses:
+ *       200:
+ *         description: Succès - Renvoie une blague.
+ *       500:
+ *         description: Erreur serveur.
+ */
 router.get('/random', jokeController.getRandomJoke);
+/**
+ * @swagger
+ * /api/blagues/{id}:
+ *   get:
+ *     summary: Récupère une blague par son ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Succès
+ *       404:
+ *         description: Blague non trouvée
+ */
 router.get('/:id', jokeController.getJokeById);
 
 module.exports = router;
